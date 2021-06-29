a090e0b3324cd221471c162e31406f43cf207426
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-06-29 15:14:42 +0700

    change config data

--- config.py
+++ config.py
@@ -1,9 +1,118 @@
+APP_ID = '956956635039988'
+APP_SECRET = 'f02e02266a2d389b89aa392dfdfaec88'
+USER_TOKEN = 'EAANmWNRMCPQBAJJppClncQdiA3QnUfDYFiavR9ZCZAcu7PH6tk3ILn4woAd6pr9lK0hdJtV3hjnfkmT9aNK3yBuSLAQRvnUZADfguTBRUgqj5UvZCtYXciQda0mrmlVzEnksOIJrNrmQxw4cfKs44MUiwKdjEApP3W3wpTMz5gZDZD'
+LUCKY_ID = '17841447551005404'
+TAMA_ID = '17841403354856241'
+
 # # DEV-WIJATAMA
 # APP_ID = '206904173852656'
 # APP_SECRET = 'd6f1a86247d110edc3968b8737d47f01'
 # USER_TOKEN = 'EAAC8LaHI9ZCABADiHHlZAJCoQ6ftkRgk8HvYOltGLSYOhRjCLnduMCHBSzZC7VrIQaDtoomufhPg9ZAZCxTx5Xb6byyqAOIwgr4yOs1MFxfCzdJpf1fgk2u2ncy792HZBKIb8FcXCbDydfwqPRw3zIsYSm5P6zl0jwadsZByc7ffXOopqFSzu4fKeKdhrZBriJsZD'
+# FB ID WIJATAMA
+# {
+#   "id": "3883217068427432", # FB ID
+#   "name": "Wijatama Diwangkara"
+# }
+# FB PAGE WIJATAMA
+#  {"name": "Tes FB APP",
+#       "id": "103898335130604"} #PAGE ID
+
+# {
+#   "instagram_business_account": {
+#     "id": "17841403354856241" # IG ID
+#   },
+#   "id": "103898335130604" # PAGE ID
+# }
+
+# {
+#   "data": [
+#     {
+#       "name": "Lucky Lake 7's Page",
+#       "access_token": "EAANmWNRMCPQBAITegLMFX53hDQQbbZA6ccoLljNcUDhX9uzPZCkV2kketZCyJW3yciqC8rSy2kmqiRPkZBMlpmvshiaZBBXIBD70WQ31mVl2qOWzxITx2zE1MJJQJNYJmTSNi8EwSUB6ztDTdF7eFJ9a7XmMfVM8pyROkuTzxlVayYvj9ALvxOooO3bZAjDuIZD",
+#       "id": "100939715495806"
+#     },
+#     {
+#       "name": "Tes FB APP",
+#       "access_token": "EAANmWNRMCPQBAAk0YbkuR5HdFxbUbBrVyiOflANVFSZCSLr1p6y9MLdEqrqQ4zLcdShN7x78AVBjCcFkKQD6TfpR6aVvMZCR6Wcugrp2sFECGZAmIflUOZCtDxs7kJ9yUWZBu3Y3ZBActxZBN5RyxUxBnOIgDV235soW4SeKT2VDQhYwt1swhsb7MhXZCBLv3wsZD",
+#       "id": "103898335130604"
+#     }
+#   ],
+#   "paging": {
+#     "cursors": {
+#       "before": "MTAwOTM5NzE1NDk1ODA2",
+#       "after": "MTAzODk4MzM1MTMwNjA0"
+#     }
+#   }
+# }
+
+# {
+#       "id": "1950994075054198",
+#       "name": "lucky lake's great group",
+#       "member_count": 2,
+#       "privacy": "OPEN"
+#     },
 
 # DEV INTEGRATED MEDIA
-APP_ID = '956956635039988'
-APP_SECRET = 'f02e02266a2d389b89aa392dfdfaec88'
-USER_TOKEN = 'EAANmWNRMCPQBAAPbu7KHkpRF2x1dZAYgcyj5LXNtj0VnTXsZC2qPct0LPPjBWZBXT6IqNepq3TeVaGglYpX7O575H7dyUiNuBCqb5ZBqxZC6qKRpZBPk12AC5XLhg9ZAnY3zZCRGzpJ9LCZAj4jBv1dWrKnMcy0xJeV9GZC3PiSzRcQKvt3ofeFZCSw5CNCZC4mcEgtMGVyyAxFbgQZDZD'
+# {
+#  "name": "Lucky Lake 7's Page",
+#  "id": "100939715495806" # page id
+# }
+# {
+#   "instagram_business_account": {
+#     "id": "17841447551005404" #ig_id
+#   },
+#   "id": "100939715495806"
+# }
+# access token = 'EAANmWNRMCPQBAKoZCTfZAlqdV46nmwuP6p2hcmEQOLUotpZAtVWOLeuCj3e8rCS0usfesULwpXt7RW3I06N9LOytZAdLMgN5su8FK6qhBmTPhIZCZB9nZCDVzLZBsMxCpICdPGOgeJ1elKEUyZCOcVokkKdqU5CIkCdf7s5kv5MtZCSKJ9W4wxURIP75r2z60G1BOe1pomDNnZCSUPGk0SfSm3j'
+
+
+# resp = {
+#     "data": [
+#     {
+#       "access_token": "EAANmWNRMCPQBAIajuZARGn6uQETWVZCEi1iGRshvgThBr9ICRuZBbhGNujXR4XoBeZAHT4dR4C2uRUzJmAgkZAyOAjjKeOLe442MtuHcXNu9k5rZBNytZC8DxQ5jjYwZC7SeS0QJispwtMAuKkvZAnyUVuCnt7pIJUtggZBrDWhOOKuAXJ10tXZBlktwhuyjcMlqtkEdKkfOxO7B7TBTArGakJi",
+#       "category": "Produk/Layanan",
+#       "category_list": [
+#         {
+#           "id": "2201",
+#           "name": "Produk/Layanan"
+#         }
+#       ],
+#       "name": "Lucky Lake 7's Page",
+#       "id": "100939715495806",
+#       "tasks": [
+#         "ANALYZE",
+#         "ADVERTISE",
+#         "MESSAGING",
+#         "MODERATE",
+#         "CREATE_CONTENT",
+#         "MANAGE"
+#       ]
+#     },
+#     {
+#       "access_token": "EAANmWNRMCPQBADHsX8TcF8alI2NERoIOTlYOYDQZCBrwNXiwf20SpMjAQlI6lhQok0zLJINzjFCimS44Jcr88ABqEwuV5oOs8eDxRLVktKKzEYnhLBBQ6Q0ZBZCW0JGZB87iXsVXRDHJZAUDngctGNyxEvD42jlftZCQNYQ9eibn7njo3z7DL4LAgAyhRb1dUUPWCSqExzfr8O84T09qwy",
+#       "category": "Perusahaan Teknologi Informasi",
+#       "category_list": [
+#         {
+#           "id": "1130035050388269",
+#           "name": "Perusahaan Teknologi Informasi"
+#         }
+#       ],
+#       "name": "Tes FB APP",
+#       "id": "103898335130604",
+#       "tasks": [
+#         "ANALYZE",
+#         "ADVERTISE",
+#         "MESSAGING",
+#         "MODERATE",
+#         "CREATE_CONTENT",
+#         "MANAGE"
+#       ]
+#     }
+#   ],
+#   "paging": {
+#     "cursors": {
+#       "before": "MTAwOTM5NzE1NDk1ODA2",
+#       "after": "MTAzODk4MzM1MTMwNjA0"
+#     }
+#   }
+# }
